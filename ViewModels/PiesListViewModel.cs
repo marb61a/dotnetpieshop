76624dbe01cc9d46603f3434f653b019a81c9a5a
martin byrne <marb61a@gmail.com>
2017-09-18 16:16:25 +0000

    added view model and controller

---
+++ ViewModels/PiesListViewModel.cs
@@ -0,0 +1,14 @@
+using dotnetpieshop.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace dotnetpieshop.ViewModels
+{
+    public class PiesListViewModel
+    {
+        public IEnumerable<Pie> Pies { get; set; }
+        public string CurrentCategory { get; set; }
+    }
+}
