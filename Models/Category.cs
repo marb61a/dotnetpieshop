8988445173fa7ea164f66845b6b3bc535b0785d6
martin byrne <marb61a@gmail.com>
2017-09-19 15:29:24 +0000

    added models

---
+++ Models/Category.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace dotnetpieshop.Models {
+    public class Category
+    {
+        public int CategoryId { get; set; }
+        public string CategoryName { get; set; }
+        public string Description { get; set; }
+        public List<Pie> Pies { get; set; }
+    }
+}
