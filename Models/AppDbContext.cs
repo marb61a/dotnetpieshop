c4045ff4977ffc9ad417cb37d4273d0e4d82cf16
martin byrne <marb61a@gmail.com>
2017-09-25 15:59:15 +0000

    adding models

---
+++ Models/AppDbContext.cs
@@ -0,0 +1,18 @@
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace dotnetpieshop.Models{
+    public class AppDbContext: DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+            
+        }
+        
+        public DbSet<Pie> Pies {get; set;}
+        public DbSet<Category> Categories {get; set; }
+    }
+}
